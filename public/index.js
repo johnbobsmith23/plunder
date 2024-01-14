d027f00cba1bf042c83dfefaf84b43bb70c696c9
JS <johnbobsmith23@gmail.com>
2024-01-14 11:06:44 -0600

    Selectable dice

--- public/index.js
+++ public/index.js
@@ -15,6 +15,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // Set up scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 300);
+camera.updateProjectionMatrix();
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 camera.position.set(0, .5, 4).multiplyScalar(7);
@@ -29,6 +30,8 @@ var renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true
 renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+var rect = renderer.domElement.getBoundingClientRect();
 
 let physicsWorld;
 // Load a GLTF model (dice)
@@ -44,7 +47,8 @@ controls.mouseButtons = {
    RIGHT: THREE.MOUSE.PAN
 }
 controls.touches = {
-   TWO: THREE.TOUCH.DOLLY_ROTATE
+   ONE: THREE.TOUCH.ROTATE,
+   TWO: THREE.TOUCH.DOLLY_PAN
 }
 
 let frames = 0, prevTime = performance.now(), highlightedObject, highlightedModelIndex, isMouseDown;
@@ -107,13 +111,14 @@ function addLight() {
 }
 
 const models = [];
+let i = 0;
 
 // Function to load a GLB model and add it to the scene
 function loadModel(url, position) {
    const loader = new GLTFLoader();
    loader.load(`./assets/${url}`, (gltf) => {
       const mesh = gltf.scene;
-      mesh.name = url;
+      mesh.name = `dice${i++}`;
       const body = new CANNON.Body({
          mass: 1,
          shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
@@ -134,6 +139,7 @@ function loadModel(url, position) {
       model.mesh.castShadow = true;
       model.mesh.receiveShadow = true;
       model.mesh.isDraggable = true;
+      model.mesh.isSelected = false;
       
       scene.add(model.mesh);
       physicsWorld.addBody(model.body);
@@ -143,65 +149,77 @@ function loadModel(url, position) {
 
 // Load three GLB models
 loadModel('move_dice.glb', new THREE.Vector3(-3, 0, 0));
-loadModel('defend_dice.glb', new THREE.Vector3(0, 0, 0));
+loadModel('defend_dice.glb', new THREE.Vector3(0, -1, 0));
 loadModel('attack_dice.glb', new THREE.Vector3(3, 0, 0));
 
 let animate = false;
+let receivingAnimation = false;
 
 function render() {
    physicsWorld.fixedStep();
    frames++;
-
-   if (animate)
+   if (animate && !receivingAnimation)
    {
       for (const model of models)
       {
-      model.mesh.position.copy(model.body.position)
-      model.mesh.quaternion.copy(model.body.quaternion)
-      let mesh = { name: model.mesh.name, position: model.mesh.position, quaternion: model.mesh.quaternion.toArray() };
-      socket.emit('updateModel', (mesh));
+         model.mesh.position.copy(model.body.position)
+         model.mesh.quaternion.copy(model.body.quaternion)
+         let mesh = { name: model.mesh.name, position: model.mesh.position, quaternion: model.mesh.quaternion.toArray() };
+         socket.emit('updateModel', (mesh));
       }
    }
 
+   if (!models.find(model => {return model.body.sleepState != 2;}))
+   {
+      receivingAnimation = false;
+      animate = false;
+   }
+   
    controls.update();
    renderer.render(scene, camera);
    requestAnimationFrame(render);
 }
 
-document.addEventListener('click', () => {
+document.addEventListener('dblclick', () => {
    throwDice();
 });
 
 function throwDice () {
+   if (animate) return;
    models.forEach((d, dIdx) => {
+      console.log(d);
+      if (!d.mesh.isSelected)
+      {
+         d.body.velocity.setZero();
+         d.body.angularVelocity.setZero();
 
-      d.body.velocity.setZero();
-      d.body.angularVelocity.setZero();
+         d.body.position = new CANNON.Vec3(6, dIdx * 1.5, 0);
+         d.mesh.position.copy(d.body.position);
 
-      d.body.position = new CANNON.Vec3(6, dIdx * 1.5, 0);
-      d.mesh.position.copy(d.body.position);
+         d.mesh.rotation.set(2 * Math.PI * Math.random(), 0, 2 * Math.PI * Math.random())
+         d.body.quaternion.copy(d.mesh.quaternion);
 
-      d.mesh.rotation.set(2 * Math.PI * Math.random(), 0, 2 * Math.PI * Math.random())
-      d.body.quaternion.copy(d.mesh.quaternion);
+         const force = 3 + 5 * Math.random();
+         d.body.applyImpulse(
+            new CANNON.Vec3(-force, force, 0),
+            new CANNON.Vec3(0, 0, .2)
+         );
 
-      const force = 3 + 5 * Math.random();
-      d.body.applyImpulse(
-         new CANNON.Vec3(-force, force, 0),
-         new CANNON.Vec3(0, 0, .2)
-      );
-
-      d.body.allowSleep = true;
-      animate = !animate;
+         d.body.allowSleep = true;
+         d.body.sleepState = 0;
+         animate = true;
+      }
    });
 }
 
+throwDice();
 render();
 
 socket.on('hello', (message) => {
    const time = performance.now();
    if ( time >= prevTime + 1000 ) {
    
-      console.log( Math.round( ( frames * 1000 ) / ( time - prevTime ) ) );
+      //console.log( Math.round( ( frames * 1000 ) / ( time - prevTime ) ) );
       document.getElementById('fps').innerHTML = frames;
       
       frames = 0;
@@ -218,6 +236,8 @@ socket.on('updatePosition', (mesh) => {
    
    model.mesh.position.copy(mesh.position);
    model.mesh.quaternion.fromArray(mesh.quaternion);
+   receivingAnimation = true;
+   animate = true;
 });
 
 
@@ -226,18 +246,10 @@ window.addEventListener('resize', () => {
    camera.aspect = window.innerWidth / window.innerHeight;
    camera.updateProjectionMatrix();
    renderer.setSize( window.innerWidth, window.innerHeight );
+   rect = renderer.domElement.getBoundingClientRect();
 });
 
 let timerGoing = false;
-controls.mouseButtons = {
-   LEFT: THREE.MOUSE.ROTATE,
-   MIDDLE: THREE.MOUSE.DOLLY,
-   RIGHT: THREE.MOUSE.PAN
-}
-controls.touches = {
-   ONE: THREE.TOUCH.ROTATE,
-   TWO: THREE.TOUCH.DOLLY_PAN
-}
 
 window.addEventListener('touchstart', () => {
    if (!timerGoing) {
@@ -260,3 +272,40 @@ window.addEventListener('touchstart', () => {
 window.addEventListener('touchend', () => {
    if (!timerGoing) controls.enableRotate = false;
 });
+
+// Allows user to pick up and drop objects on-click events
+window.addEventListener("click", (event) => {
+   // If NOT 'holding' object on-click, set container to <object> to 'pickup' the object.
+   updateMousePosition(event);
+   raycaster.setFromCamera(mouse, camera);
+   const found = raycaster.intersectObjects(models.map(model => model.mesh));
+   if (found.length)
+   {
+      let current = found[0].object;
+      while (current.parent.parent !== null) {
+         current = current.parent;
+      }
+      if (!current.isSelected) {
+         current.children[0].material.emissive.setHex(0x00ff00);
+         current.isSelected = true;
+         console.log(current);
+      } else {
+         current.children[0].material.emissive.setHex(0x000000);
+         current.isSelected = false;
+      }
+   }
+});
+
+
+function onMouseMove(event) {
+   // Convert mouse coordinates to normalized device coordinates
+   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
+document.addEventListener('mousemove', updateMousePosition);
+
+function updateMousePosition(event) {   
+   mouse.x = ( ( event.clientX - rect.left ) / ( rect.right - rect.left ) ) * 2 - 1;
+   mouse.y = - ( ( event.clientY - rect.top ) / ( rect.bottom - rect.top) ) * 2 + 1;
+}
